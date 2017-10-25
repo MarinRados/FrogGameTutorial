5f25c42402859a694b0621e84e38e4ede30b63e9
Marin Radoš <marinradosh@gmail.com>
2017-10-25 14:11:48 +0200

    game done

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BirdMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	private NavMeshAgent birdAgent;
+	private Animator birdAnimator;
+	[SerializeField]
+	private RandomSoundPlayer birdFootsteps;
+
+	// Use this for initialization
+	void Start () {
+		birdAgent = GetComponent<NavMeshAgent>();
+		birdAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		birdAgent.SetDestination (target.position);
+
+		float speed = birdAgent.velocity.magnitude;
+		birdAnimator.SetFloat ("Speed", speed);
+
+		if (speed > 0) {
+			birdFootsteps.enabled = true;
+		} else {
+			birdFootsteps.enabled = false;
+		}
+	}
+}
---
+++ Assets/Scripts/FlySpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject flyPrefab;
+
+	[SerializeField]
+	private int totalFlyMinimum = 12;
+
+	private float spawnArea = 23f;
+
+	public static int totalFlies;
+
+	// Use this for initialization
+	void Start () {
+		totalFlies = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		while (totalFlies < totalFlyMinimum) {
+
+			totalFlies++;
+
+			float positionX = Random.Range (- spawnArea, spawnArea);
+			float positionZ = Random.Range (- spawnArea, spawnArea);
+
+			Vector3 flyPostition = new Vector3 (positionX, 2f, positionZ);
+
+			Instantiate (flyPrefab, flyPostition, Quaternion.identity);
+		}
+	}
+}
